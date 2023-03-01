fa949d77d1068a2fd1429bcf4c8409a96868c7ad
IlkhamBurkhonov <ilkhomjonburkhonov207@gmail.com>
2023-03-01 15:05:16 +0500

    order bug fixed

--- components/Order/order.js
+++ components/Order/order.js
@@ -3,28 +3,44 @@ import React from "react";
 import Image from "next/image";
 import Money from "../../public/Assets/Images/HeaderAndHeroImg/Money.svg";
 import UzCard from "../../public/Assets/Images/HeaderAndHeroImg/UZcard.svg";
+import Link from "next/link";
 
 function Order() {
   return (
     <div>
+      <div className="bg-white flex items-center w-full pt-1.5 mx-20 pb-1.5 mb-[70px] px-8">
+        <Link href="#" className="flex items-center">
+          <h2 className="font-normal text-[#109EF4] text-xs ml-2.5">Главная</h2>
+        </Link>
+        <span className="ml-2.5 text-[#109EF4] ">/</span>
+        <Link href="#">
+          <h2 className="font-normal text-[#109EF4] text-xs ml-2.5">Корзина</h2>
+        </Link>
+        <span className="ml-2.5 text-[#109EF4] ">/</span>
+        <Link href="#">
+          <h2 className="font-normal text-[#109EF4] text-xs ml-2.5">
+            Оформить заказ
+          </h2>
+        </Link>
+      </div>
       <div>
         <h2 className="text-center font-bold text-2xl">Oформлению заказа</h2>
-        <div className="grid grid-cols-3 mx-20 pr-20">
+        <div className="grid grid-cols-3 mx-20 mt-12">
           <div className="col-span-2">
             <h1 className=" text-lg font-500">1. Личные данные</h1>
-            <div className="flex justify-between">
-              <label>
+            <div className="flex justify-between gap-5">
+              <label className="w-full">
                 <h2 className="mt-5 text-[#24283A] font-[550]">Имя</h2>
                 <input
-                  className="w-[408px] h-12 border px-3 rounded-lg mt-3"
+                  className="w-full h-12 border px-3 rounded-lg mt-3"
                   placeholder="Введите ваше имя"
                 />
               </label>
-              <label>
+              <label className="w-full ">
                 <h2 className="mt-5 text-[#24283A] font-[550]">
                   Номер телефона
                 </h2>
-                <div className=" flex items-center pl-4  mt-3 w-[408px] h-48 text-base rounded-lg p-1 outline-none border  border-gray-input_radius mb-4 sm:mb-6">
+                <div className=" flex items-center pl-4  mt-3 w-full h-48 text-base rounded-lg p-1 outline-none border  border-gray-input_radius mb-4 sm:mb-6">
                   <Image
                     src={"/Assets/Images/BuyAll/Flag.svg"}
                     className="w-auto h-auto"
@@ -48,36 +64,82 @@ function Order() {
               </label>
             </div>
             <h2 className="font-500 text-lg mt-1">2. Выберите способ оплаты</h2>
-            <div className="flex justify-between mt-5">
-              <button className="flex justify-center items-center w-[260px] border rounded-md h-14 gap-2 hover:bg-[#605dec14]">
+            <div className="flex justify-between mt-5 gap-7">
+              <button className="flex justify-center items-center w-full border rounded-md h-14  hover:bg-[#605dec14]">
                 <Image src={Money} /> Наличными
               </button>
-              <button className="flex justify-center items-center w-[260px] border rounded-md h-14 gap-2 hover:bg-[#605dec14]">
+              <button className="flex justify-center items-center w-full border rounded-md h-14  hover:bg-[#605dec14]">
                 <Image src={UzCard} /> Uzcard/Humo
               </button>
-              <button className=" flex justify-center items-center w-[260px] border rounded-md h-14 gap-2 hover:bg-[#605dec14]">
+              <button className=" flex justify-center items-center w-full border rounded-md h-14  hover:bg-[#605dec14]">
                 <Image src={UzCard} /> Payme/Click
               </button>
             </div>
             <h3 className="font-500 text-lg mt-7">3. Ваши детали доставки</h3>
-            <div className="flex justify-between mt-5">
-              <label className="">
+            <div className="flex justify-between mt-5 gap-5">
+              <label className="w-full">
                 <h2>Регион</h2>
-                <select className="w-[408px] border h-12 mt-3 rounded-lg px-4">
+                <select className="w-full border h-12 mt-3 rounded-lg px-4">
                   <option className="text-[#B4B6B8]">Выбирите</option>
                   <option>Uzbekistan</option>
                 </select>
               </label>
-              <label>
+              <label className="w-full">
                 <h2>Город</h2>
-                <select className="w-[408px] border h-12 mt-3 rounded-lg px-4">
+                <select className="w-full border h-12 mt-3 rounded-lg px-4">
                   <option className="text-[#B4B6B8]">Выбирите</option>
                   <option>Qoqon</option>
                 </select>
               </label>
             </div>
+            <div className="grid grid-cols-8 gap-7 mt-7">
+              <label className="col-span-6">
+                <h2 className="font-500">Address</h2>
+                <input
+                  className="border w-full h-12 mt-3 rounded-lg placeholder:text-[#B4B6B8] px-4"
+                  placeholder="Введите ваше адрес"
+                />
+              </label>
+              <label className="col-span-2">
+                <h2 className="font-500">Этаж</h2>
+                <input
+                  className="border w-full h-12 mt-3 rounded-lg placeholder:text-[#B4B6B8] px-4"
+                  placeholder="Если есть"
+                />
+              </label>
+            </div>
+            <div className="mt-7">
+              <h2 className="font-500">Комментарий</h2>
+              <input
+                className="border w-full h-12 mt-3 rounded-lg placeholder:text-[#B4B6B8] px-4"
+                placeholder="Комментарий"
+              />
+            </div>
+            <button className="w-full h-12 text-white bg-[#2B3D90] text-center text-lg rounded-lg mt-10">
+              Оформить заказ
+            </button>
           </div>
-          <div>Summa</div>
+          <div className="border ml-6 p-7 rounded-md  h-[366px]">
+            <div className=" flex justify-between">
+              <h2 className=" text-lg">В корзине (2)</h2>
+              <p className="text-lg"> 1 290 000 сум</p>
+            </div>
+            <div className="flex justify-between  mt-3">
+              <h2 className="text-lg">Скидка:</h2>
+              <p className="text-lg"> 1 290 000 cyм</p>
+            </div>
+            <div className="flex justify-between mt-3">
+              <h2 className="text-lg">Доставка:</h2>
+              <p className="text-lg"> 20 000 cyм</p>
+            </div>
+
+            <h1 className=" border-y-1 py-7 pl-4 mt-7 text-[22px]">
+              Всего к оплате : <span className="pl-4"> 1 290 000 sum</span>
+            </h1>
+            <button className="px-[42px] w-full py-3 mb-8 mt-5 bg-[#2B3D90] rounded-xl text-white text-lg">
+              Изменить заказ
+            </button>
+          </div>
         </div>
       </div>
     </div>
